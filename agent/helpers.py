7b0b5de733cfef997cd8510d63c2bf855ab93949
Zack Littke-Smith <zack.littkesmith@gmail.com>
2019-05-30 23:14:54 -0700

    adding agent folder, helpers skeleton

---
+++ agent/helpers.py
@@ -0,0 +1,35 @@
+
+'''
+Returns a list of valid actions given a state
+'''
+def getValidActionsInState(state):
+	# TODO implement
+	pass
+
+'''
+Minify an action to a unique string
+'''
+def compressAction(action):
+	# TODO implement
+	pass
+
+'''
+Decompress a compressed action
+'''
+def decompressAction(compressed_action):
+	# TODO implement
+	pass
+
+'''
+Minify a state object to a unique string
+'''
+def compressState(state):
+	# TODO implement
+	pass
+
+'''
+Decompress a compressed state
+'''
+def decompressState(compressed_state):
+	# TODO implement
+	pass
