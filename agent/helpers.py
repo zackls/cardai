d95a2e8062db920e80051f82650628107608cbaa
Zack Littke-Smith <zack.littkesmith@gmail.com>
2019-06-02 01:09:07 -0700

    adding agent folder, helpers skeleton

--- agent/helpers.py
+++ agent/helpers.py
@@ -7,6 +7,15 @@ def getValidActionsInState(state):
 	pass
 
 '''
+Finds the closest state to the state passed in.
+Works by selecting a random set of already-observed
+states from q and computing the closest.
+'''
+def getClosestObservedState(state, q, tries=100):
+	# TODO implement
+	pass
+
+'''
 Minify an action to a unique string
 '''
 def compressAction(action):
